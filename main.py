b3bfc003ba5da4e99012624b7ee426c10b785cc1
MasterCode200 <104267880+MasterCode200@users.noreply.github.com>
2022-11-13 12:11:53 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+firstnumber=int(input("Type in the first number"))
+secondnumber=int(input("Type in the second number"))
+add=firstnumber+secondnumber
+substract=firstnumber-secondnumber
+multiplication=firstnumber*secondnumber
+division=firstnumber//secondnumber
+remainder=firstnumber%secondnumber
+power=firstnumber**secondnumber
+print("the addition of the two numbers is ",add)
+print("the substraction of the two numbers is ",substract)
+print("the multiplication of the two numbers is ",multiplication)
+print("the quotient of the two numbers is ",division,"and the remainder is ",remainder)
+print("the square of the numbers is",power)
